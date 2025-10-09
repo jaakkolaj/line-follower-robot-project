ad757357dc6308979ee3efe5c225aad4e82a9050
Luukas Laitinen <166003611+lluukas@users.noreply.github.com>
2025-10-09 11:46:55 +0300

    Update ROBOROCK.ino
    Tosta

--- ROBOROCK/ROBOROCK.ino
+++ ROBOROCK/ROBOROCK.ino
@@ -1,17 +1,21 @@
-/*
- */
 #define vasenEtu 7
 #define oikeaEtu 12
 #define vasenTaka 11
 #define oikeaTaka 8
-#define enableOikea 9
 #define enableVasen 6
+#define enableOikea 9
 
 #define ir1 A0
 #define ir2 A1
 #define ir3 A2
 #define ir4 A3
 #define ir5 A4
+
+// --- PD Controller constants ---
+float Kp = 25.0;  // Proportional gain
+float Kd = 10.0;  // Derivative gain
+float previousError = 0;
+int baseSpeed = 120;  // Base motor speed (0–255)
 
 void setup() {
   pinMode(oikeaEtu, OUTPUT);
@@ -20,135 +24,103 @@ void setup() {
   pinMode(vasenTaka, OUTPUT);
   pinMode(enableVasen, OUTPUT);
   pinMode(enableOikea, OUTPUT);
+
   pinMode(ir1, INPUT);
   pinMode(ir2, INPUT);
   pinMode(ir3, INPUT);
   pinMode(ir4, INPUT);
   pinMode(ir5, INPUT);
+
   Serial.begin(9600);
 }
-//vauhti asteikolla 0-255
+
 void loop() {
-  int sensori1 = digitalRead(ir1);
-  int sensori2 = digitalRead(ir2);
-  int sensori3 = digitalRead(ir3);
-  int sensori4 = digitalRead(ir4);
-  int sensori5 = digitalRead(ir5);
+  int s1 = !digitalRead(ir1);
+  int s2 = !digitalRead(ir2);
+  int s3 = !digitalRead(ir3);
+  int s4 = !digitalRead(ir4);
+  int s5 = !digitalRead(ir5);
 
-  Serial.print(!sensori1);
-  Serial.print(!sensori2);
-  Serial.print(!sensori3);
-  Serial.print(!sensori4);
-  Serial.println(!sensori5);
+  Serial.print(s1);
+  Serial.print(s2);
+  Serial.print(s3);
+  Serial.print(s4);
+  Serial.println(s5);
 
+  // --- Compute position error ---
+  // Sensor weighting: leftmost = -2, rightmost = +2
+  int position = (-2 * s1) + (-1 * s2) + (0 * s3) + (1 * s4) + (2 * s5);
+  int activeSensors = s1 + s2 + s3 + s4 + s5;
+  float error = 0;
+  if (activeSensors != 0) error = (float)position / activeSensors;
 
-  //Keskimmäinen lukee mustaa jotenka mennään eteenpäin
-  if((sensori1 == 1) && (sensori2 == 1) && (sensori3 == 0) && (sensori4 == 1) && (sensori5 == 1))
-  {
-    analogWrite(enableVasen, 120); //you can adjust the speed of the motors from 0-255
-    analogWrite(enableOikea, 120); //you can adjust the speed of the motors from 0-255
+  // --- PD Control ---
+  float derivative = error - previousError;
+  float correction = (Kp * error) + (Kd * derivative);
+  previousError = error;
+
+  int leftSpeed = baseSpeed + correction;
+  int rightSpeed = baseSpeed - correction;
+
+  // Clamp speeds to [0, 255]
+  leftSpeed = constrain(leftSpeed, 0, 255);
+  rightSpeed = constrain(rightSpeed, 0, 255);
+
+  // --- Your existing logic (still active) ---
+  // Keskimmäinen lukee mustaa jotenka mennään eteenpäin
+  if ((s1 == 0) && (s2 == 0) && (s3 == 1) && (s4 == 0) && (s5 == 0)) {
+    analogWrite(enableVasen, leftSpeed);
+    analogWrite(enableOikea, rightSpeed);
     digitalWrite(oikeaEtu, HIGH);
     digitalWrite(vasenEtu, HIGH);
     digitalWrite(oikeaTaka, LOW);
     digitalWrite(vasenTaka, LOW);
   }
-  
-  //Keskivasen lukee mustaa jotenka käännytään vasemalle
-  if((sensori1 == 1) && (sensori2 == 0) && (sensori3 == 1) && (sensori4 == 1) && (sensori5 == 1))
-  {
-    analogWrite(enableOikea, 150); //you can adjust the speed of the motors from 0-255
-    analogWrite(enableVasen, 150); //you can adjust the speed of the motors from 0-255
-    digitalWrite(oikeaEtu, LOW);
+
+  // Keskivasen lukee mustaa jotenka käännytään vasemmalle
+  if ((s1 == 0) && (s2 == 1) && (s3 == 0) && (s4 == 0) && (s5 == 0)) {
+    analogWrite(enableVasen, leftSpeed);
+    analogWrite(enableOikea, rightSpeed);
+    digitalWrite(oikeaEtu, HIGH);
     digitalWrite(vasenEtu, LOW);
+    digitalWrite(oikeaTaka, LOW);
+    digitalWrite(vasenTaka, HIGH);
+  }
+
+  // Keskioikea lukee mustaa jotenka käännytään oikealle
+  if ((s1 == 0) && (s2 == 0) && (s3 == 0) && (s4 == 1) && (s5 == 0)) {
+    analogWrite(enableVasen, leftSpeed);
+    analogWrite(enableOikea, rightSpeed);
+    digitalWrite(oikeaEtu, LOW);
+    digitalWrite(vasenEtu, HIGH);
     digitalWrite(oikeaTaka, HIGH);
     digitalWrite(vasenTaka, LOW);
-    
-
   }
-  
-  //Vasen lukee mustaa jotenka käännytään vasemalle
-  if((sensori1 == 0) && (sensori2 == 1) && (sensori3 == 1) && (sensori4 == 1) && (sensori5 == 1))
-  {
-    analogWrite(enableOikea, 180);
-    analogWrite(enableVasen, 180);
+
+  // Vasen lukee mustaa (suurempi korjaus vasemmalle)
+  if ((s1 == 1) && (s2 == 0) && (s3 == 0) && (s4 == 0) && (s5 == 0)) {
+    analogWrite(enableVasen, leftSpeed);
+    analogWrite(enableOikea, rightSpeed);
     digitalWrite(oikeaEtu, HIGH);
     digitalWrite(vasenEtu, LOW);
     digitalWrite(oikeaTaka, HIGH);
     digitalWrite(vasenTaka, LOW);
-    
-
   }
 
-  //keskioikea lukee mustaa jotenka käännytään oikealle
-  if((sensori1 == 1) && (sensori2 == 1) && (sensori3 == 1) && (sensori4 == 0) && (sensori5 == 1))
-  {
-    analogWrite(enableOikea, 150);
-    analogWrite(enableVasen, 150);
-    digitalWrite(oikeaEtu, LOW);
-    digitalWrite(vasenEtu, HIGH);
-    digitalWrite(oikeaTaka, LOW);
-    digitalWrite(vasenTaka, LOW);
-  }
-
-  //Oikea lukee mustaa jotenka käännytään oikealle
-  if((sensori1 == 1) && (sensori2 == 1) && (sensori3 == 1) && (sensori4 == 1) && (sensori5 == 0))
-  {
-    analogWrite(enableOikea, 180);
-    analogWrite(enableVasen, 180);
+  // Oikea lukee mustaa (suurempi korjaus oikealle)
+  if ((s1 == 0) && (s2 == 0) && (s3 == 0) && (s4 == 0) && (s5 == 1)) {
+    analogWrite(enableVasen, leftSpeed);
+    analogWrite(enableOikea, rightSpeed);
     digitalWrite(oikeaEtu, LOW);
     digitalWrite(vasenEtu, HIGH);
     digitalWrite(oikeaTaka, LOW);
     digitalWrite(vasenTaka, HIGH);
   }
 
-  //Keskimmäinen ja keskioikee lukee mustaa jotenka käännytään oikealle
-  if((sensori1 == 1) && (sensori2 == 1) && (sensori3 == 0) && (sensori4 == 0) && (sensori5 == 1))
-  {
-    analogWrite(enableOikea, 150);
-    analogWrite(enableVasen, 150);
-    digitalWrite(oikeaEtu, LOW);
-    digitalWrite(vasenEtu, HIGH);
-    digitalWrite(oikeaTaka, LOW);
-    digitalWrite(vasenTaka, LOW);
-  }
-
-  //Keskimmäinen ja keskivasen lukee mustaa jotenka käännytään vasemmalle
-  if((sensori1 == 1) && (sensori2 == 0) && (sensori3 == 0) && (sensori4 == 1) && (sensori5 == 1))
-  {
-    analogWrite(enableOikea, 150);
-    analogWrite(enableVasen, 150);
-    digitalWrite(oikeaEtu, LOW);
-    digitalWrite(vasenEtu, LOW);
-    digitalWrite(oikeaTaka, HIGH);
-    digitalWrite(vasenTaka, LOW);
-  }
-
-  //Keskimmäinen ja siitä vasemmalle lukee mustaa jotenka käänytään vasemmalle
-  if((sensori1 == 0) && (sensori2 == 0) && (sensori3 == 0) && (sensori4 == 1) && (sensori5 == 1))
-  {
-   
-    analogWrite(enableOikea, 150);
-    analogWrite(enableVasen, 150);
-    digitalWrite(oikeaEtu, LOW);
-    digitalWrite(vasenEtu, LOW);
-    digitalWrite(oikeaTaka, HIGH);
-    digitalWrite(vasenTaka, LOW);
-  }
-
-  //Keskimmäisestä oikealle kaikki lukee mustaa jotenka käännytään oikealle
-  if((sensori1 == 1) && (sensori2 == 1) && (sensori3 == 0) && (sensori4 == 0) && (sensori5 == 0))
-  {
-    analogWrite(enableOikea, 150);
-    analogWrite(enableVasen, 150); 
-    digitalWrite(oikeaEtu, LOW);
-    digitalWrite(vasenEtu, HIGH);
-    digitalWrite(oikeaTaka, LOW);
-    digitalWrite(vasenTaka, LOW);
-  }
-
-  //Kaikki sensorit lukee mustaa eli pysähtyy
-  if((sensori1 == 1) && (sensori2 == 1) && (sensori3 == 0) && (sensori4 == 1) && (sensori5 == 1))
-  {
+  // Kaikki sensorit mustalla (pysähdys)
+  if ((s1 == 1) && (s2 == 1) && (s3 == 1) && (s4 == 1) && (s5 == 1)) {
+    analogWrite(enableVasen, 0);
+    analogWrite(enableOikea, 0);
     digitalWrite(oikeaEtu, LOW);
     digitalWrite(vasenEtu, LOW);
     digitalWrite(oikeaTaka, LOW);
